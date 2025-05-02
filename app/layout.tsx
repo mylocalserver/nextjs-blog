df1e43f086401336cbf878a0b54760336fb81b2e
masayoshi_S <spfy2f89@carol.ocn.ne.jp>
2025-05-02 10:25:29 +0900

    Profile Image

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import Navbar from './components/Navbar'
+import MyProfilePic from './components/MyProfilePic'
 
 export default function RootLayout({
   children,
@@ -10,6 +11,7 @@ export default function RootLayout({
     <html lang="en">
       <body className="dark:bg-slate-800">
         <Navbar / >
+        <MyProfilePic / >
         {children}
       </body>
     </html>
