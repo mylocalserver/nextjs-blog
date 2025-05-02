4769ac4365215626b94d4e78858cf02ef1de6020
masayoshi_S <spfy2f89@carol.ocn.ne.jp>
2025-05-02 09:40:25 +0900

    Navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+import Navbar from './components/Navbar'
 
 export default function RootLayout({
   children,
@@ -8,6 +9,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="dark:bg-slate-800">
+        <Navbar / >
         {children}
       </body>
     </html>
